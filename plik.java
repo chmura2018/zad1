801e0823c4dfa57dcd5bd2fb72f5c83658ec037d
chmura2018 <45427974+chmura2018@users.noreply.github.com>
2018-12-09 23:05:04 +0100

    Update plik.java

--- plik.java
+++ plik.java
@@ -2,77 +2,92 @@ import java.sql.*;
 
 public class plik {
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://db:10.0.10.3:3306/zad1";
+   static final String DB_URL = "jdbc:mysql://10.0.10.3:3306/baza";
 
    static final String USER = "pkalasa";
-   static final String PASS = "password";
+   static final String PASS = "root";
    
-   public static void main(String[] args) {
+   public static void main(String[] args) throws InterruptedException{
    Connection conn = null;
    Statement stmt = null;
-   try{
-	   System.out.println("Check if table in base exist");
+   Boolean baseExist = false;
+   Boolean login = true;
+   String sql;
+   
+    while(login) {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");            
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            login = false; 
+        }catch(SQLException se){
+    Thread.sleep(10000);
+        }catch(Exception e){
+    Thread.sleep(10000);
+        }finally{
+    System.out.println("Connecting to database...");
+        }
+    }
+    
+    try{        
+      System.out.println("Check if table in base exist");
         DatabaseMetaData md = conn.getMetaData();
         ResultSet rs = md.getTables(null, null, "Persons", null);
         while (rs.next()) {
             System.out.println("Table Exist");
             baseExist = true;
         }
-      Class.forName("com.mysql.jdbc.Driver");
+        rs = null;  
+        if(!baseExist){
+          System.out.println("Creating Table");
+          stmt = conn.createStatement();
+          sql = "CREATE TABLE Persons (PersonID int, LastName varchar(50), FirstName varchar(50), Address varchar(50), City varchar(50) )";
+          stmt.executeUpdate(sql);
+          stmt = null;
+        }     
+        stmt = conn.createStatement();
+        System.out.println("Inserting Data to Table");
+        sql = "INSERT INTO Persons (PersonID, LastName, FirstName, Address, City) VALUES (1, 'Kowalski', 'Jan', 'DLuga','Lublin'), (2, 'Nowak', 'Edyta', 'Jasna','Warszawa'), (3, 'Kozak', 'Mariusz', 'Debowa','Gdansk')";
+        stmt.executeUpdate(sql);   
+        stmt = null;
+     
+        stmt = conn.createStatement();
+        sql = "SELECT PersonID, FirstName, LastName, Address, City FROM Persons";
+        rs = stmt.executeQuery(sql);
 
-      System.out.println("Czekaj na połączenie...");
-      conn = DriverManager.getConnection(DB_URL,USER,PASS);
-if(!baseExist){
-		stmt = conn.createStatement();
-			System.out.println("Creating Table");
-			stmt = conn.createStatement();
-			sql = "CREATE TABLE Tabela(id int, kolumna1 varchar(255), kolumna2 varchar(255), kolumna3 varchar(255))";
-			stmt.executeUpdate(sql);
-			stmt = null;
-}
-		
-		stmt = conn.createStatement();
-			System.out.println("Inserting Data to Table");
-			sql = "INSERT INTO Tabela (id, kolumna1, kolumna2, kolumna3) VALUES (1, 'tekst1', 'tekst2', 'tekst3'), (2, 'tresc1', 'tresc2', 'tresc3'), (3, 'tresc1', 'tresc2', 'tresc3')";
-			stmt.executeUpdate(sql);   
-			stmt = null;
-
-		stmt = conn.createStatement();
-			sql = "SELECT id, kolumna1, kolumna2, kolumna3 FROM Tabela";
-			result = stmt.executeQuery(sql);
-
-
-      while(result.next())
-	{
-        int id  = result.getInt("id");
-        String kol1 = result.getString("kolumna1");
-        String kol2 = result.getString("kolumna2");
-		String kol3 = result.getString("kolumna3");
-	
-		System.out.print("ID: " +id);
-		System.out.print(",kolumna1:" + kol1);
-		System.out.print(",kolumna2:" + kol2);
-		System.out.print(",kolumna3:" + kol3);
-
-	}
-      result.close();
-      stmt.close();
-      conn.close();
-	  
-     }catch(SQLException se){
+      while(rs.next()){
+        int id  = rs.getInt("PersonID");
+          String first = rs.getString("FirstName");
+          String last = rs.getString("LastName");
+      String address = rs.getString("Address");
+      String city = rs.getString("City");
+    
+          System.out.println("ID: " + id);
+          System.out.println(", First: " + first);
+          System.out.println(", Last: " + last);
+      System.out.println(", Address: " + address);
+      System.out.println(", City: " + city);
+        }
+        
+        rs.close();
+        stmt.close();
+        conn.close();
+      
+    }catch(SQLException se){
         se.printStackTrace();
-     }catch(Exception e){
-        e.printStackTrace();
-     }finally{
-        try{
-           if(stmt!=null) stmt.close();
+    }catch(Exception e){
+      e.printStackTrace();
+    }finally{
+    try{
+          if(stmt!=null) stmt.close();
         }catch(SQLException se2){
+        
         }
         try{
-           if(conn!=null) conn.close();
+        if(conn!=null) conn.close();
         }catch(SQLException se){
-           se.printStackTrace();
-      }
-   }
- }
+        se.printStackTrace();
+        }
+    }
+  }
 }
